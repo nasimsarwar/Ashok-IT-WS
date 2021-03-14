2020925c05fed1adc62f88c06b85eddd52fa3cc9
nasimsarwar <wdigilabs@gmail.com>
2021-03-14 19:32:20 +0530

    application.propeties file update and Websecurity

--- src/main/java/com/aws/codestar/projecttemplates/security/WebSecurity.java
+++ src/main/java/com/aws/codestar/projecttemplates/security/WebSecurity.java
@@ -51,7 +51,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 	
 	public AuthenticationFilter  getAuthenticationFilter() throws Exception{
 		final AuthenticationFilter filter = new AuthenticationFilter(authenticationManager());
-		filter.setFilterProcessesUrl("/users/login");
+		filter.setFilterProcessesUrl("users/login");
 		return filter;
 	}
 	
